7340cd6b31f479be015f45d0b1b379cb42b5a285
eggbasegroup <jon.haener@gmail.com>
2013-06-02 23:10:21 +0800

    HTML cleaned up

--- js/grabber.js
+++ js/grabber.js
@@ -1,47 +1,53 @@
 // grabber.js
 
 (function(window) {
-	// templates
-	var templ_cleanedText   		= Handlebars.compile( $("#cleaned-text-template").html() ),
-		templ_features 				= Handlebars.compile( $("#pa-order-template").html() ),
-		templ_techData 				= Handlebars.compile( $("#techData-template").html() ),
-		$leftSection 				= $('.section-left'),
-	  	$rightSection 				= $('.section-right'),
-		$rightSectionInitPosLeft	= $('.section-right').offset().left;
-		$rightSectionInitPosTop		= $('.section-right').offset().top;
+  // templates
+  var templ_cleanedText        = Handlebars.compile( $("#cleaned-text-template").html() ),
+      templ_features           = Handlebars.compile( $("#pa-order-template").html() ),
+      templ_techData           = Handlebars.compile( $("#techData-template").html() ),
+      $leftSection             = $('.section-left'),
+	  $rightSection            = $('.section-right'),
+	  $rightSectionInitPosLeft = $('.section-right').offset().left;
+	  $rightSectionInitPosTop  = $('.section-right').offset().top;
 	
 	// helper-extract text
-	function grabText(txt) {
-		  // if input is not string return false 
-		  if (typeof txt !== "string") {
-			  return false;
-		  }
-		  // holds results
-		  var resObj = {},
-		  // trim text input
-		  text = txt.trim(),
-		  // GREPs
-		  //grep_header 	= /[^(<.>)$](\w*\d*)[^<\/.>)$]/ig,
-		  grep_header 	= /([\w\s?]*)(?=(<\/header))/ig,
-		  grep_features = /([\w\s?]*)(?=(<\/features))/ig,
-		  grep_techData = /([\w\s?]*)(?=(<\/techdata))/ig, // (^|\s)Blah(\s|$)		
-		  // extractor function
-		  extract = function(grep, txt) {
-			  var resArr = [];
-			  if (grep.test(txt) === true) {
-				  resArr = txt.match(grep);					
-			  } else {
-				  resArr[0] = "Error: Check text settings/missing tags";
-			  }
-			  return resArr.join(" ");
-		  };
-		  // return extracted results		
-		  return {
-			  header	:	extract (grep_header, text),
-			  techData 	:	extract (grep_techData, text),
-			  features 	:	extract (grep_features, text)
-		  }
-	}
+  function grabText(txt) {
+      // if input is not string return false 
+      if (typeof txt !== "string") {
+        return false;
+      }
+      // holds results
+      var resObj = {},
+      // trim text input
+      text = txt.trim(),
+      // GREPs
+      //grep_header 	= /[^(<.>)$](\w*\d*)[^<\/.>)$]/ig,
+      grep_header 	= /([\w\s?]*)(?=(<\/header))/ig,
+      grep_features = /([\w\s?]*)(?=(<\/features))/ig,
+      grep_techData = /([\w\s?]*)(?=(<\/techdata))/ig, // (^|\s)Blah(\s|$)		
+      // extractor function
+      extract = function(grep, txt) {
+          var resArr = [];
+          if (grep.test(txt) === true) {
+            resArr = txt.match(grep);					
+          } else {
+	        resArr[0] = "Error: Check text settings/missing tags";
+          }
+          return resArr.join(" ");
+      };
+      // return extracted results		
+      return {
+        header	:	extract (grep_header, text),
+        techData 	:	extract (grep_techData, text),
+        features 	:	extract (grep_features, text)
+      }
+  }
+	
+	
+	// Continue space aligning code from here
+	// Continue space aligning code from here
+	// Continue space aligning code from here
+	
 	
 	// retrieve saved Items
 	function loadSavedItems(){
